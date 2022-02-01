9298d2b29d3031e4f4853ed72f8b2f987b441e86
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2022-02-01 12:26:30 +0100

    Min första php-fil

---
+++ first.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP</h1>
+    <p>Ursprungligen var det en förkortning för Personal Home Page.</p>
+    <p>Numera är det en akronym för "PHP: Hypertext Preprocessor"</p>
+    <?php 
+        $namn = "Tobias";
+        echo "<p>Mitt första <strong>PHP-script</strong>.</P>
+        <p>Jag heter $namn.</p>";
+    ?>
+
+</body>
+</html>
---
+++ php-kommandon.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+echo skriver ut text.
+Variabelnamn i php börjar alltid med $.
+
+<h2>Samma som i c#</h2>
+<ul>
+    <li>Kommentarer skrivs efter //</li>
+    <li>php-satser avslutas med ;</li>
+</ul>
+
+
+<?php
+    echo "<h1>Echo</h1>";
+    $namn = "Tobias";
+    echo "Jag heter $namn";
+?>
+
+
+<?php //En kommentar ?>
+
+</body>
+</html>
